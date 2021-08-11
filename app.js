e2943610ee8a80ec84b3296a033051220dad6023
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-08-12 00:28:11 +0100

    display data functionality added

--- app.js
+++ app.js
@@ -1,53 +1,87 @@
-const searchBar = document.getElementById('search')
-const submitBtn = document.getElementById('submit')
+const searchBar = document.getElementById('search');
+const submitBtn = document.getElementById('submit');
 
-submitBtn.addEventListener('click', handleSearch)
+const locationElement = document.querySelector('.location');
+const tempElement = document.querySelector('.temperature');
+const minElement = document.querySelector('.min');
+const maxElement = document.querySelector('.max');
+const feelsElement = document.querySelector('feels');
+const humidityElement = document.querySelector('.humidity');
+const weatherElement = document.querySelector('.weather');
+const descriptionElement = document.querySelector('.description');
+const windElement = document.querySelector('.description');
 
-async function getWeather(location){
-    try {
-        const response = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=d8bb32a119bbf160e2545fa1ef9df520`)
-        const weatherData = await response.json();
-        // ... do something with the data, but for now:
+submitBtn.addEventListener('click', handleSearch);
 
-        console.log(weatherData)
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=d8bb32a119bbf160e2545fa1ef9df520`
+    );
+    const weatherData = await response.json();
+    // ... do something with the data, but for now:
 
-        processData(weatherData)
+    console.log(weatherData);
 
-        return weatherData
+    processData(weatherData);
 
-    } catch (error) {
-        console.log('ERROR: uh oh, spagghetti-ohs')
-    }
-} 
-
+    return weatherData;
+  } catch (error) {
+    console.log('ERROR: uh oh, spagghetti-ohs');
+  }
+}
 
 function processData(data) {
-    // console logged the required data for the app
 
-    // Temperature and humidity
-    console.log(`Location is: ${data.name}`)
-    console.log(`Temperature is: ${data.main.temp}°C`)
-    console.log(`Maximum temperature is: ${data.main.temp_max}°C`)
-    console.log(`Minimum temperature is: ${data.main.temp_min}°C`)
-    console.log(`Feels like: ${data.main.feels_like}°C`)
-    console.log(`Humidity is: ${data.main.humidity}%`)
+  class Weather {
+    constructor(data) {
+      this.location = data.name;
+      this.temp = data.main.temp;
+      this.min = data.main.min_temp;
+      this.max = data.main.max_temp;
+      this.feels = data.main.feels_like;
+      this.humidity = data.main.humidity;
+      this.weather = data.weather[0].main;
+      this.description = data.weather[0].description;
+      this.wind = data.wind.speed;
+    }
+  }
 
-    // Weather and description
-    console.log(`Weather is: ${data.weather[0].main}`)
-    console.log(`Weather description: ${data.weather[0].description}`)
+  const newWeather = new Weather(data)
 
-    // Wind
+  displayData(newWeather)
 
-    console.log(`Wind speed is: ${data.wind.speed}mph`)
+
 }
-
 
 function handleSearch(e) {
-    e.preventDefault()
-    const location = searchBar.value
-    getWeather(location)
+  e.preventDefault();
+  const location = searchBar.value;
+  getWeather(location);
+  searchBar.value = '';
 }
 
+function displayData(data) {
+  // ...
 
-getWeather('Widnes')
 
+  // console logged the required data for the app
+
+  // Temperature and humidity
+  locationElement.textContent = `Location is: ${data.location}`;
+  tempElement.textContent = `Temperature is: ${data.temp}°C`;
+  minElement.textContent = `Maximum temperature is: ${data.max}°C`;
+  maxElement.textContent = `Minimum temperature is: ${data.min}°C`;
+  //feelsElement.textContent = `Feels like: ${data.feels}°C`;
+  humidityElement.textContent = `Humidity is: ${data.humidity}%`;
+
+  // Weather and description
+  weatherElement.textContent = `Weather is: ${data.weather}`;
+  descriptionElement.textContent = `Weather description: ${data.description}`;
+
+  // Wind
+
+  windElement.textContent = `Wind speed is: ${data.wind}mph`;
+}
+
+getWeather('Widnes');
